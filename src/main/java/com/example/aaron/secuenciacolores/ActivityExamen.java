f5502376b3cf9cb8231f0a84a848fdc8551b0f7b
Aaron <amartinezdasilva@danielcastelao.org>
2016-12-16 12:08:11 +0100

    probando metodos en la nueva activity

--- src/main/java/com/example/aaron/secuenciacolores/ActivityExamen.java
+++ src/main/java/com/example/aaron/secuenciacolores/ActivityExamen.java
@@ -1,9 +1,25 @@
 package com.example.aaron.secuenciacolores;
 
+import android.widget.Toast;
+
 /**
  * Created by Aaron on 16/12/2016.
  */
 
 public class ActivityExamen extends MainActivity{
-    
+    void Comprobar(){
+        if(cont==4) {
+            String x = randomColor.toString();
+            String y = elegirColor.toString();
+
+            if (x.equalsIgnoreCase(y)) {
+
+                Toast.makeText(getApplicationContext(), "ACERTASTE", Toast.LENGTH_SHORT).show();
+
+            } else {
+                Toast.makeText(getApplicationContext(), "FALLO", Toast.LENGTH_SHORT).show();
+            }
+
+        }
+    }
 }
